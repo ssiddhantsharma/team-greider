7b082fe1b4da0af9b8ea3c163d0de30df0124595
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-02 01:41:00 +0530

    Update stage_0_Siddhant.R

--- stage_0_Siddhant.R
+++ stage_0_Siddhant.R
@@ -1,9 +1,8 @@
 name <- 'Siddhant Sharma'
 email <- "siddhaantsharma.ss@gmail.com"
-language <- 'R'
-biostack <- "Transcriptomics"
 slackuser <- "@Siddhant"
-data <- data.frame(Information=c(name, email, language, biostack, slackuser))
-rownames(data) <- c('NAME: ', 'E-MAIL: ', 'USED LANGUAGE: ', 'BIOSTACK: ', 'SLACK USERNAME: ')
+biostack <- "Transcriptomics"
+data <- data.frame(Information=c(name, email, slackuser, biostack))
+rownames(data) <- c('NAME: ', 'E-MAIL: ', 'SLACK USERNAME: ', 'BIOSTACK: ')
 colnames(data) <- NULL
 print(data)

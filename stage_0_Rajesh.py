93d4f329956f15249dc04d8ba27825990d5ab389
rajesh4472 <88301709+rajesh4472@users.noreply.github.com>
2021-08-02 12:09:22 +0530

    Add files via upload
    File uploaded

---
+++ stage_0_Rajesh.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#my personal details
+name="Rajesh_chavhan"
+email="rajshrichavhan15@gmail.com"
+slackuser="@Rajesh"
+biostack="genomics"
+print(f"{name}, {email},{slackuser},{biostack}")

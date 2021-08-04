74cb3c9cad13a4a364df497a8fe27ec3dcf2005d
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-05 00:08:54 +0530

    Create stage_0_Siddhant.jl

---
+++ stage_0_Siddhant.jl
@@ -0,0 +1,4 @@
+list = ["NAME: Siddhant Sharma","EMAIL: siddhaantsharma.ss@gmail.com", "SLACKUSER: @Siddhant", "BIOSTACK: Medicinal chemistry and cheminformatics","TWITTER: @sidd2508", "HAMMINGD: 5"]
+ for i in list
+       println(i)
+       end

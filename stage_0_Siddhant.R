6486fa670cc599b5d8f706a06096dbe7ba20d83c
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-04 23:27:40 +0530

    Update stage_0_Siddhant.R

--- stage_0_Siddhant.R
+++ stage_0_Siddhant.R
@@ -1,8 +1,7 @@
-name <- 'Siddhant Sharma'
+name <- "Siddhant Sharma"
 email <- "siddhaantsharma.ss@gmail.com"
 slackuser <- "@Siddhant"
 biostack <- "Medicinal chemistry and cheminformatics"
-data <- data.frame(Information=c(name, email, slackuser, biostack))
-rownames(data) <- c('NAME: ', 'E-MAIL: ', 'SLACK USERNAME: ', 'BIOSTACK: ')
-colnames(data) <- NULL
-print(data)
+twitter <- "@ssiddhant_"
+my_details <- c(name, email, slackuser, biostack, twitter)
+cat(paste(my_details, collapse = ','))

d8254a98a3bdfdfe740217b93f2fc53cde24209c
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-05 00:10:52 +0530

    Update stage_0_Siddhant.R

--- stage_0_Siddhant.R
+++ stage_0_Siddhant.R
@@ -1,7 +1,8 @@
-name <- "Siddhant Sharma"
-email <- "siddhaantsharma.ss@gmail.com"
-slackuser <- "@Siddhant"
-biostack <- "Medicinal chemistry and cheminformatics"
-twitter <- "@ssiddhant_"
-my_details <- c(name, email, slackuser, biostack, twitter)
+NAME <- "Siddhant Sharma"
+EMAIL <- "siddhaantsharma.ss@gmail.com"
+SLACKUSER <- "@Siddhant"
+BIOSTACK <- "Medicinal chemistry and cheminformatics"
+TWITTER <- "@sidd2508"
+HAMMINGD <- "5"
+my_details <- c(NAME, EMAIL, SLACKUSER, BIOSTACK, TWITTER, HAMMINGD)
 cat(paste(my_details, collapse = ','))

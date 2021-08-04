d8a411f3da94cfb5db275826c012a9ad3b62cd64
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-05 00:46:57 +0530

    Add files via upload

---
+++ csv-populator.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+git clone "https://github.com/ssiddhantsharma/team-greider.git"
+Pwd
+cd ./team-greider
+Pwd
+ls
+for file in $(ls);
+do
+	if [[ $file == *.py ]];
+	then
+		python $file >> ../team-greider.csv
+	elif [[ $file == *.sh ]];
+	then
+		bash $file >> ../team-greider.csv
+	elif [[ $file == *.R ]];
+	then
+		Rscript $file >> ../team-greider.csv
+	elif [[ $file == *.c || $file == *.cpp || $file == *.cs]];
+	then
+		g++ $file 
+		./a.out >> ../team-greider.csv
+		rm a.out
+	elif [[ $file == *.rb ]];
+	then
+		ruby $file >> ../team-greider.csv
+	elif [[ $file == *.jl ]];
+	then
+		julia $file >> ../team-greider.csv
+	elif [[ $file == *.java ]];
+        then
+                javac $file
+		fname=`echo $file | cut -d\. -f1`
+		java $fname >> ../team-greider.csv
+	fi
+done

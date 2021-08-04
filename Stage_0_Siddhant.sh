d4eb218b1b19a5050f91667b630a624587db366c
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-04 23:50:10 +0530

    Create Stage_0_Siddhant.sh

---
+++ Stage_0_Siddhant.sh
@@ -0,0 +1,7 @@
+name='Siddhant Sharma'
+email='siddhaantsharma.ss@gmail.com'
+slackuser='@Siddhant'
+biostack='Medicinal chemistry and cheminformatics'
+twitter='@ssiddhant_'
+
+echo $name','$email', '$slackuser', $biostack','$twitter

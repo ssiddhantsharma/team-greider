7223ce286094639187b5de45c4396238b67890e6
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-05 22:39:37 +0530

    Update Stage_0_Siddhant.sh

--- Stage_0_Siddhant.sh
+++ Stage_0_Siddhant.sh
@@ -6,3 +6,25 @@ TWITTER='@sidd2508'
 HAMMINGD='5'
 
 echo $NAME','$EMAIL', '$SLACKUSER', '$BIOSTACK','$TWITTER', '$HAMMINGD
+
+##!/bin/bash
+#main() {
+	#if [ $# -eq 0 ] || [ $# -eq 1 ]; then
+		#echo "Usage: siddhant_hamming.sh <string1> <string2>"
+		#exit 1
+    #else
+        #first=$1
+        #second=$2
+        #distance=0
+        #for (( i=0; i<${#first}; ++i )); do
+            #if [ ${first:$i:1} != ${second:$i:1} ]; then
+                #let "distance++"
+            #fi
+        #done
+
+        #echo $distance
+	#fi
+
+#}
+
+#main "$@"

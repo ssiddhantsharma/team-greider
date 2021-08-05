040a84fc6a18e1cba429e61c6eaf1febf33e1a0f
Derinsolar <88289596+Derinsolar@users.noreply.github.com>
2021-08-05 02:13:11 +0100

    Add files via upload

---
+++ stage_0_aderinsola.py
@@ -0,0 +1,15 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Name: Aderinsola Adebisi")
+Name: Aderinsola Adebisi
+>>> print ("Email: adebisiaderinsola@gmail.com")
+Email: adebisiaderinsola@gmail.com
+>>> print ("Slack username: @Aderinsola")
+Slack username: @Aderinsola
+>>> print ("Biostack: Drug Development and medicinal chemistry")
+Biostack: Drug Development and medicinal chemistry
+>>> print ("Twitter username: Derinsola")
+Twitter username: Derinsola
+>>> print ("Hamming distance: 2")
+Hamming distance: 2
+>>> 

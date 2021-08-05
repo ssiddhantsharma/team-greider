89d59450ef49a635d225c2d1d92810d26130b75f
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-05 22:56:40 +0530

    Update Stage_0_Anjali.R

--- Stage_0_Anjali.R
+++ Stage_0_Anjali.R
@@ -1,2 +1,2 @@
 #info
-cat("NAME: Anjali\nEMAIL: anjalisaanvi2001@gmail.com\nSlACKUSERR: @Anjali\nBIOSTACK: Genomics\n,TWITTER: @AnjSO5\n, HAMMINGD: 3")
+cat("NAME: Anjali Sharma\nEMAIL: anjalisaanvi2001@gmail.com\nSlACKUSERR: @Anjali\nBIOSTACK: Genomics\n,TWITTER: @AnjSO5\n, HAMMINGD: 3")

e8b76d269feef5640cef7f9264887b7e98c31155
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-07 15:35:35 +0530

    Create stage_0_Siddhant.js

---
+++ stage_0_Siddhant.js
@@ -0,0 +1,13 @@
+let Details = {
+    NAME: ": Siddhant Sharma",
+    EMAIL: ": siddhaantsharma.ss@gmail.com",
+    SLACK_USERNAME: ": @Siddhant"
+    BIOSTACK: ": Medicinal chemistry and cheminformatics",
+    TWITTER: ": @sidd2508"
+    HAMMINGD: ": 5"
+    
+};
+
+for (let [key, value] of Object.entries(Details)){
+    console.log(key, value);
+}

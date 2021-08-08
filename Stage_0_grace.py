340fbbe809c6b871caf29d2590ab5cbcee827dff
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-08 20:13:36 +0530

    Update Stage_0_grace.py

--- Stage_0_grace.py
+++ Stage_0_grace.py
@@ -1,22 +1,10 @@
-name ="Ajayi Grace"
-email = "gracejeta08@gmail.com"
-slack_user_name = "@grace"
-biostack = "Genomics"
-twitter_handle ="Gracy_ldn"
-print("NAME: "+name)
-print("EMAIL: "+email)
-print("SLACK USERNAME: "+slack_user_name)
-print("BIOSTACK: "+biostack)
-print("TWITTER HANDLE: "+twitter_handle)
+#personaldetails
+print("NAME: Ajayi Grace \nE-MAIL: gracejeta08@gmail.com \nSLACK USERNAME: @grace \nBIOSTACK: Genomics \nTwitter Handle: grape") 
+def hamming_distance(a,b):
+    count=0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            count +=1
+    return count
 
-def hammingDist(str1, str2):
-    i = 0
-    count = 0
-    while(i < len(str1)):
-        if(str1[i] != str2[i]):
-            count += 1
-            i += 1
-            return count
-str1 = "SLACK USERNAME"
-str2 = "TWITTER HANDLE"
-print(hammingDist(str1, str2))
+print(hamming_distance('@grace','@grape')) 

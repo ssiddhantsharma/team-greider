14a91bcc6ff84cced5a62d7f848ce24be846eebf
Margaret Chifwete <71388707+chifwete@users.noreply.github.com>
2021-08-08 08:36:00 +0300

    Add files via upload
    Corrected version of my personal details

--- stage_0_Margaret.R
+++ stage_0_Margaret.R
@@ -1,5 +1,7 @@
-NAME: Margaret Chifwete
+print("NAME: Margaret Chifwete
 E-MAIL: moseleychichi@gmail.com
 SLACK USERNAME: @margaret
 BIOSTACK:  Genomics
-  
+Twitter: @moseleychichi
+Hamming: (4)")
+

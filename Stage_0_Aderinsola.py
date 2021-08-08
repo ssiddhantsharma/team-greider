5950fd08576c0943900f4ec4224da8827a0dbb04
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-08 20:10:53 +0530

    Update Stage_0_Aderinsola.py

--- Stage_0_Aderinsola.py
+++ Stage_0_Aderinsola.py
@@ -1,6 +1,11 @@
-print("Name: Aderinsola Adebisi")
-print("Email: adebisiaderinsola@gmail.com")
-print("Slack username: @Aderinsola")
-print("Biostack: Drug Development")
-print("Twitter username: @Derinsolar")
-print("Hamming distance: 2")
+
+#personaldetails
+print("NAME: Aderinsola Adebisi \nE-MAIL: adebisiaderinsola@gmail.com \nSLACK USERNAME: @Aderinsola \nBIOSTACK: Drug Development \nTwitter Handle: @Derinsolar") 
+def hamming_distance(a,b):
+    count=0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            count +=1
+    return count
+
+print(hamming_distance('@Aderinsola','@Derinsolar')) 

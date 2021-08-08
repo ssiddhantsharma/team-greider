7327353fafe79278f7e5e8a42982f5db07655c9d
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-08 20:04:55 +0530

    Update stage_0_Wealth.py

--- stage_0_Wealth.py
+++ stage_0_Wealth.py
@@ -1,25 +1,10 @@
 #personaldetails
-name = "Wealth Okete"
-email = "wealth.okete@gmail.com"
-slack_username = "@Wealth"
-biostack = "Genomics"
-twitter_handle = "@Wealty"
+print("NAME:Wealth Okete \nE-MAIL: wealth.okete@gmail.com \nSLACK USERNAME: @Wealth \nBIOSTACK: Genomics \nTwitter Handle: @Wealty")
+def hamming_distance(a,b):
+    count=0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            count +=1
+    return count
 
-print ("NAME: "+name)
-print ("EMAIL: "+email)
-print ("SLACK USERNAME: "+slack_username)
-print ("BIOSTACK: "+biostack)
-print ("TWITTER HANDLE: "+twitter_handle)
-
-#hammingdistance
-def hammingDist (str1, str2):
-    i = 0
-    count = 0
-    while(i < len(str1)):
-        if(str1[i] != str2[i]):
-             count += 1
-             i += 1
-        return count
-str1 = "slack_username"
-str2 = "twitter_handle"
-print(hammingDist(str1, str2))
+print(hamming_distance('@Wealth','@Wealty'))

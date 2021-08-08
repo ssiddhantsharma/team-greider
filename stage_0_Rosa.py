0b6201cbf4c41a1e10c743d2d319d219539bf344
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2021-08-08 20:06:41 +0530

    Update stage_0_Rosa.py

--- stage_0_Rosa.py
+++ stage_0_Rosa.py
@@ -1,18 +1,10 @@
-#personal info
+#personaldetails
+print("NAME: Rosa Laura Depau \nE-MAIL: rosalauradepau@yahoo.it \nSLACK USERNAME: @Rosa \nBIOSTACK: Transcriptomics \nTwitter Handle: @Roza") 
+def hamming_distance(a,b):
+    count=0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            count +=1
+    return count
 
-print("NAME: Rosa Laura Depau")
-print("E-MAIL: rosalauradepau@yahoo.it")
-print("SLACK USERNAME: @Rosa")
-print("BIOSTACK: Transcriptomics")
-print("TWITTER: @Roza")
-from scipy.spatial.distance import hamming
-
-#define arrays
-x = ['@','R','o','s','a']
-y = ['@','R','o','z','a']
-
-#calculate Hamming distance between the two arrays
-result = hamming(x,y) * len (x)
-
-#print the haming distance between the two vectors 
-print('Hamming distance between Slack and twitter username:',result)
+print(hamming_distance('@Rosa','@Roza')) 
